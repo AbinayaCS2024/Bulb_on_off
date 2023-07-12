f0531f5e464bb570e850c87087cfd744caa08d57
AbinayaCS2024 <132117220+AbinayaCS2024@users.noreply.github.com>
2023-07-12 11:08:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+function bulb_on(){
+    img.src="./image/bulbon.jpg";
+}
+function bulb_of(){
+    img.src="./image/bulbof.jpg";
+
+}
